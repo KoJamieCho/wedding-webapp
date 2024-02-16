0357eaf15ce45ff8931a9f4700eb96987a3e3662
Minho Cho <whalsght@polarium.co.kr>
2024-02-16 18:17:07 +0900

    code add

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="footer">
+    <div class="contants">
+      <div>Created on The Knot</div>
+      <div class="bottomTitle">Getting married? Create your wedding website for free.</div>
+      <div>Your Privacy Choices</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 5%;
+  .contants {
+    color: #231f20;
+    text-align: center;
+    font-size: 12px;
+    letter-spacing: 0px;
+    line-height: 1.75;
+    .bottomTitle {
+      margin-top: 30px;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main">
+    <div class="mainTitle">
+      <div>
+        <h1>our</h1>
+        <h1>wedding</h1>
+      </div>
+    </div>
+    <div class="subTitle">
+      <div>
+        <h2>micheal & minsu</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  position: fixed;
+  top: 10%;
+
+  .mainTitle {
+    text-align: center;
+    line-height: 1;
+    margin: 20px 0;
+    color: #231f20;
+    font-size: 40px;
+    letter-spacing: 8px;
+    padding-bottom: 10px;
+    text-transform: uppercase;
+  }
+  .subTitle {
+    line-height: 1;
+    color: #231f20;
+    font-size: 40px;
+    letter-spacing: 8px;
+    text-transform: uppercase;
+    text-align: center;
+  }
+}
+</style>
